501009d1a11d21e22d141abf74f4c48db572f405
Chris Nunez <cenunez47@gmail.com>
2017-09-17 19:10:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,9 +90,9 @@ class TicTacToe
     @board[index] = player
   end
 
-  def winner(board)
-    if winning_combo = won?(board)
-      board[winning_combo.first]
+  def winner
+    if winning_combo = won?
+      @board[winning_combo.first]
     end
   end
 
