48622e405e842b85e37d72e0072cb21e09a7e1c4
Chris Nunez <cenunez47@gmail.com>
2017-09-17 19:11:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def valid_move?(board, index)
+  def valid_move?(index)
     index.between?(0,8) && !position_taken?(@board, index)
   end
 
